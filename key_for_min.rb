18de73db98d24a0c24979aa84f80960f4b44b3f9
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-15 00:40:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,11 +7,12 @@
 def key_for_min_value(name_hash)
   min_val = 0 
   min_ky = nil 
-  name_hash.each do |nut,number|
+  name_hash.each do |key,number|
     if number < min_val 
       min_val = number
-      min_ky = nut 
-    end 
+      min_ky = key 
+    end
+    return min_ky
   end 
-  return min_ky
+  
 end
