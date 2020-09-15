bc150a63fe6d91af2c81e87783ed4c3f933d1980
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-15 00:38:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,12 +1,7 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-name_hash = {
-  "walnuts"=> 4, 
-  "hazelnuts"=> 6,
-  "pistachios"=> 34
-  
-}
+
 
 
 def key_for_min_value(name_hash)
