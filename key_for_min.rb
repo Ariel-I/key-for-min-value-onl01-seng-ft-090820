7d85d0e365e002a4438649f7aeed3f44796ea26f
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-15 00:33:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -14,4 +14,8 @@ def key_for_min_value(name_hash)
   lowet_key = nil 
   name_hash.each do |nut,number|
     if number < lowest_value 
+      lowest_value = number
+      lowest_key = key 
+    end 
+  end 
 end
